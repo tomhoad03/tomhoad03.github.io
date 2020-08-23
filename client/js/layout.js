c358d462f26c51bdc7649a9e36edb0ba4c81ce24
tomhoad03 <tdh1g19@soton.ac.uk>
2020-08-23 13:20:37 +0100

    Darker navbar - aim for transitions

--- client/js/layout.js
+++ client/js/layout.js
@@ -1,22 +1,21 @@
 function loadHeader(webpage) {
     let header = '<!-- Displays a navbar at the top of the page -->\n' +
-        '            <nav class="navbar navbar-expand fixed-top scrolling-navbar navbar-light bg-light shadow">\n' +
+        '            <nav class="navbar navbar-expand fixed-top scrolling-navbar shadow">\n' +
         '                <div id="navbar" class="container">\n' +
-        '                    <a class="navbar-brand">Tom Hoad</a>\n' +
+        '                    <a class="navbar-brand" href="index.html">Tom Hoad</a>\n' +
         '\n' +
         '                    <!-- Displays links to other webpages -->\n' +
         '                    <div class="collapse navbar-collapse justify-content-between">\n' +
-        '                        <div class="navbar-nav">\n' +
-        '                            <a id="link-index" class="nav-item nav-link" href="index.html">Homepage</a>\n' +
-        '                            <a id="link-code" class="nav-item nav-link" href="#">Programming</a>\n' +
-        '                            <a id="link-reviews" class="nav-item nav-link" href="#">Film Reviews</a>\n' +
+        '                        <div id="webpage-links" class="navbar-nav">\n' +
+        '                            <a id="link-programming" class="nav-item nav-link" href="programming.html">Programming</a>\n' +
+        '                            <a id="link-reviews" class="nav-item nav-link" href="reviews.html">Film Reviews</a>\n' +
         '                            <a id="link-youtube" class="nav-item nav-link" href="youtube.html">YouTube</a>\n' +
         '                            <a id="link-spotify" class="nav-item nav-link" href="spotify.html">Spotify</a>\n' +
         '                            <a id="link-gaming" class="nav-item nav-link disabled" href="#">Gaming</a>\n' +
         '                        </div>\n' +
         '\n' +
         '                        <!-- Displays links to other platforms -->\n' +
-        '                        <div class="navbar-nav">\n' +
+        '                        <div id="social-icons" class="navbar-nav">\n' +
         '                            <a class="nav-item nav-link" href="https://github.com/tomhoad03">\n' +
         '                                <i class="fa fa-github mr-1"></i>\n' +
         '                            </a>\n' +
@@ -33,12 +32,15 @@ function loadHeader(webpage) {
         '                                <i class="fa fa-spotify mr-1"></i>\n' +
         '                            </a>\n' +
         '\n' +
-        '                            <button class="btn btn-outline-secondary ml-3">Download</button>\n' +
+        '                            <button id="download-button" class="btn btn-outline-light ml-3">Download</button>\n' +
         '                        </div>\n' +
         '                    </div>\n' +
         '                </div>\n' +
         '            </nav>';
 
     $('#header').append(header);
-    $('#link-' + webpage).addClass(' active');
+
+    if (webpage != null) {
+        $('#link-' + webpage).addClass(' active');
+    }
 }
