b624f3ee5d81871806c26107f8a74990ab11078c
tomhoad03 <tdh1g19@soton.ac.uk>
2020-08-25 16:39:13 +0100

    Added review model and controller

---
+++ server/Review.java
@@ -0,0 +1,13 @@
+public class Review {
+    private String title;
+    private int year;
+    private int score;
+    private String description;
+
+    public Review(String title, int year, int score, String description) {
+        this.title = title;
+        this.year = year;
+        this.score = score;
+        this.description = description;
+    }
+}
