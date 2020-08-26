0f8a6218d46bf47a6e356b26dddebd0024bf4f4a
tomhoad03 <tdh1g19@soton.ac.uk>
2020-08-26 23:08:29 +0100

    Search bar and dropdown

--- client/js/reviews.js
+++ client/js/reviews.js
@@ -10,16 +10,24 @@ function Review(title, year, score, description) {
     }
 }
 
+/* Ways to expand in the future:
+1. Sort function (by year, by rating)
+2. Give films id and link to imdb
+3. Colour based on genre
+4. Twitter API, future reviews get added here
+5. Search function - find films easier
+ */
 function addReview() {
     // Reads the JSON database storing reviews
-    $.getJSON("client/json/reviewsDB.json", function(data) {
+    $.getJSON("client/json/reviewsDB.json", function (data) {
         for (let key in data) {
             if (data.hasOwnProperty(key)) {
+
                 for (let group in data[key]) {
                     if (data[key].hasOwnProperty(group)) {
                         // Displays the review on the webpage
                         let review = new Review(data[key][group].title, data[key][group].year, data[key][group].score, data[key][group].description);
-                        $('.card-columns').append("<div class=\"card\">\n" +
+                        $('.card-columns').append("<div class=\"card shadow\">\n" +
                             "                    <div class=\"card-body\">\n" +
                             "                        <h5 class=\"card-title\">" + review.title + " (" + review.year + ")" + "</h5>\n" +
                             "                        <h6>" + review.scoreText() + "</h6>\n" +
