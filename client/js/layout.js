a0ed5be7b903b48f535eeb65580e898f17269d8c
tomhoad03 <tdh1g19@soton.ac.uk>
2020-08-29 12:08:01 +0100

    Added a basic footer and changed navbar colours

--- client/js/layout.js
+++ client/js/layout.js
@@ -1,3 +1,4 @@
+/*
 // Runs on scroll
 window.onscroll = function() {
     scrollFunction()
@@ -22,3 +23,4 @@ function scrollFunction() {
         $("#social-icons *").css("color", accentColor);
     }
 }
+*/
