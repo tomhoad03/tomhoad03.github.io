763ef355c8cebb91d1fe8fbee59fcad4361781ea
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2022-12-29 22:10:01 +0000

    Dynamic date

---
+++ client/js/index.js
@@ -0,0 +1,14 @@
+function main() {
+    let birthday = new Date("2000-11-03");
+    let currentDate = new Date();
+
+    let age = currentDate.getFullYear() - birthday.getFullYear();
+
+    if (currentDate.getMonth() < birthday.getMonth()) {
+        age--;
+    } else if ((currentDate.getMonth() === birthday.getMonth()) && (currentDate.getDate() < birthday.getDate())) {
+        age--;
+    }
+
+    document.getElementById("age-text").innerHTML = age;
+}
